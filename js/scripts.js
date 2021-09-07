b7d33b3d1aa1de45cc0938fe59ce922ee8c87567
simongartner-ourliving <simon.gartner@ourliving.se>
2021-09-07 23:56:17 +0200

    test 2

--- js/scripts.js
+++ js/scripts.js
@@ -211,7 +211,7 @@ $(document).ready(function () {
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
-        console.log('hej')
+        console.log(data)
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
@@ -219,7 +219,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxm9nPrt6U3I1xY1OCrftH3hecKPZzAwtEA6aUtxlY297ByMucW-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbxB24sCgWmHDQ8YgKU2YgliZksJwdXUF6A1sAV9S9fAiuimShqxWGGuhPxhSyMOzLn2cw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
