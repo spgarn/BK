715450b6a9f2a23b4396ddcaa77575f9be754ef8
simongartner-ourliving <simon.gartner@ourliving.se>
2021-09-07 11:26:11 +0200

    test old url

--- js/scripts.js
+++ js/scripts.js
@@ -219,7 +219,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('http://script.google.com/macros/s/AKfycbxm9nPrt6U3I1xY1OCrftH3hecKPZzAwtEA6aUtxlY297ByMucW/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
