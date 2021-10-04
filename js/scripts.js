4e99a9cdb06e31de663098f30d5fbba571ad6da9
simongartner-ourliving <simon.gartner@ourliving.se>
2021-10-04 20:37:32 +0200

    test

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Ledsen</strong>, din kod var inkorrekt.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUk4WmlVyh0RbyVN1gqTrCwlHQLy63adhlwb-74ayufP2NuV8Oqw1nsCeUhb4VGNsT0A/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyc1Go70ZPZMp3yMMKgH6Txks_iDJB-XuxXUxjCkAQFHH4XI2suQ7v9fEEFQR5rKJUOsA/exec', data)
                 .done(function (data) {
                     if (data.result === "error") {
                         $('#alert-wrapper').html(alert_markup('danger', data.message));
